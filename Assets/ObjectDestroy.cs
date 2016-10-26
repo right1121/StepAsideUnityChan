d6b7743ba822440d707151872776eba4fa9aed77
MsakiHoshino <right1121@hotmail.co.jp>
2016-10-26 16:09:56 +0900

    アイテムの破棄
    ユニティちゃんの位置によりアイテムを破棄するスクリプトを各Prefabにアタッチしました。

---
+++ Assets/ObjectDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroy : MonoBehaviour {
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		unitychan = GameObject.Find("unitychan");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < unitychan.transform.position.z - 10 ) {
+			Destroy (this.gameObject);
+		}
+	}
+}
